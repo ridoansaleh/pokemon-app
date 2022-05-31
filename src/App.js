f8d2667952a029ba1cf0ed1378aa2fd8b368a49c
Ridoan Nasution <ridoan.nasution@aia.com>
2022-06-01 06:20:56 +0700

    render pokemon list and add infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Card, Tag } from "antd";
+import useInfiniteScroll from "./hooks/useInfiniteScroll";
+import "./App.css";
+const { Meta } = Card;
+
+const colors = ["#f50", "#2db7f5", "#87d068", "#108ee9"];
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [isFetching, setIsFetching] = useInfiniteScroll(getPokemonList);
+  const [nextPokemonUrl, setNextPokemonUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=25"
+  );
+
+  useEffect(() => {
+    getPokemonList();
+  }, []);
+
+  function getPokemonList() {
+    fetch(nextPokemonUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        let pokemonList = [];
+        const pokemonPromises = data.results.map((pokemon) =>
+          fetch(pokemon.url).then((res) => res.json())
+        );
+        Promise.allSettled(pokemonPromises).then((datas) => {
+          pokemonList = datas.map((d) => d.value);
+          setTimeout(() => {
+            setPokemons((prevPokemons) => [...prevPokemons, ...pokemonList]);
+            setNextPokemonUrl(data.next);
+            setIsFetching(false);
+          }, 900);
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h3>Pokemon App</h3>
       </header>
-    </div>
+      <main style={{ overflow: "auto" }}>
+        <div className="pokemon-list">
+          {pokemons.map((pokemon, index) => (
+            <Card
+              key={index}
+              hoverable
+              cover={
+                <img alt={pokemon.name} src={pokemon.sprites.front_shiny} />
+              }
+            >
+              <Meta
+                title={`#${pokemon.id} ${pokemon.name}`}
+                description={pokemon.types.map((d, idx) => (
+                  <Tag key={idx} color={colors[Math.floor(Math.random() * 3)]}>
+                    {d.type.name}
+                  </Tag>
+                ))}
+              />
+            </Card>
+          ))}
+          {isFetching && "Fetching more pokemon..."}
+        </div>
+      </main>
+    </>
   );
 }
 
---
+++ src/hooks/useInfiniteScroll.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+function useInfiniteScroll(callback) {
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (!isFetching) return;
+    callback();
+  }, [isFetching]);
+
+  function handleScroll() {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >=
+        document.documentElement.scrollHeight &&
+      !isFetching
+    ) {
+      setIsFetching(true);
+    }
+  }
+
+  return [isFetching, setIsFetching];
+}
+
+export default useInfiniteScroll;
